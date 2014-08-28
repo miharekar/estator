ed656a318be15032d962fc4dd291140c70742bf8
Miha Rekar <info@mr.si>
2014-08-28 12:39:16 +0200

    initial commit - forgot to commit WIP :grin:

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2.0.beta1'
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 5.0.0.beta1'
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier'
+# Use CoffeeScript for .js.coffee assets and views
+gem 'coffee-rails', '~> 4.0.0'
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
+# Use Rails Html Sanitizer for HTML sanitization
+gem 'rails-html-sanitizer', '~> 1.0'
+# Use Unicorn as the app server
+gem 'unicorn'
+
+gem 'nokogiri'
+gem 'nokogiri-styles'
+
+gem 'webmock'
+gem 'vcr'
+
+group :development, :test do
+  gem 'binding_of_caller'
+  gem 'better_errors'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,105 @@
+require 'open-uri'
+
+class HomeController < ApplicationController
+  def index
+    @estates = {
+      bolha: bolha,
+      nepremicnine: nepremicnine,
+      salomon: salomon
+    }
+    render json: @estates
+  end
+
+  private
+
+  def bolha
+    url = 'http://www.bolha.com/nepremicnine/stanovanja?adTypeH=02_Oddam/&location=Osrednjeslovenska/Ljubljana/&hasImages=Oglasi%20s%20fotografijami&viewType=30&priceSortField=400%7C700&renovatedYear=2012%20in%20ve%C4%8D%7C%7C2010%20do%202011'
+    html = Nokogiri::HTML(open(url))
+    html.css('#list .adGridContent').map{ |estate|
+      estate_url = "http://www.bolha.com#{estate.at_css('a')['href']}"
+      cache estate_url do
+        estate_html = Nokogiri::HTML(open(estate_url))
+        {
+          basic: estate_html.at_css('.oglas-podatki').to_s,
+          extra: estate_html.at_css('.oglas-podatki-ostalo').to_s,
+          price: estate.at_css('.price').text,
+          all_images: estate_html.css('a.gal').map{ |img| img['href'] },
+          size: get_bolha_size(estate_html),
+          date: get_bolha_date(estate_html),
+          url: estate_url
+        }
+      end
+    }.sort_by{ |e| e[:date] }.reverse
+  end
+
+  def nepremicnine
+    url = 'http://www.nepremicnine.net/nepremicnine.html?n=1&d=197&p=3&r=14&c1=400&c2=700&s=16'
+    html = Nokogiri::HTML(open(url))
+    html.css('.oglas_container').map{ |estate|
+      estate_url = "http://www.nepremicnine.net#{estate.at_css('a')['href']}"
+      next if estate.at_css('img')['src'] == '/images/n-1.jpg'
+      cache estate_url do
+        estate_html = Nokogiri::HTML(open(estate_url))
+        {
+          basic: estate_html.at_css('.main-data table').to_s,
+          extra: estate_html.at_css('.opis').to_s,
+          price: estate.at_css('.cena').text,
+          all_images: estate_html.css('a.rsImg').map{ |a| a.attr('data-rsbigimg') },
+          size: estate.at_css('.velikost').text.gsub(',', '.').to_f,
+          url: estate_url
+        }
+      end
+    }.compact
+  end
+
+  def salomon
+    url = 'http://www.salomon.si/oglasi/nepremicnine/stanovanje?q=&mmType=1&filters=1471s-83851x1472s-90256x447m-27555&onPage=100&priceFrom=400&priceTo=700'
+    html = Nokogiri::HTML(open(url))
+    html.css('#advertList article:not(.banner20)').map{ |estate|
+      estate_url = "http://www.salomon.si#{estate.at_css('a')['href']}"
+      cache estate_url do
+        estate_html = Nokogiri::HTML(open(estate_url))
+        {
+          basic: estate_html.at_css('#advAttr table').to_s,
+          extra: estate_html.at_css('#moreAttr article').to_s,
+          price: estate.at_css('.price').text,
+          all_images: estate_html.css('.thumbsList a').map{ |a| a['href'] },
+          size: get_salomon_size(estate_html),
+          url: estate_url
+        }
+      end
+    }
+  end
+
+  def get_bolha_date html
+    html.css('.adInfo p').each do |row|
+      if row.text =~ /Vpisano/
+        return Time.parse(row.children.last)
+      end
+    end
+  end
+
+  def get_bolha_size html
+    html.css('.oglas-podatki tr').each do |row|
+      if row.text =~ /Velikost/
+        return row.at_css('b').text.gsub(',', '.').to_f
+      end
+    end
+  end
+
+  def get_salomon_size html
+    rows = html.css('#advAttr tr')
+    rows.each_with_index do |row, i|
+      if row.text =~ /Površina/
+        return rows[i+1].children.last.text.gsub(',', '.').to_f
+      end
+    end
+  end
+
+  def cache url
+    url = url.split('/').values_at(2, -1).join('/')
+    VCR.use_cassette url do
+      yield
+    end
+  end
+end
