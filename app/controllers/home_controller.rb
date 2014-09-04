0da1afd15271e89756f8e0f8c2b5bac47c4d3274
Miha Rekar <info@mr.si>
2014-09-04 18:32:23 +0200

    fix URLs to 399 because logic
    also closes #1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 require 'open-uri'
 
 class HomeController < ApplicationController
-  BOLHA_URL = 'http://www.bolha.com/nepremicnine?adTypeH=02_Oddam/&viewType=30&priceSortField=400%7C700&hasImages=Oglasi%20s%20fotografijami&location=Osrednjeslovenska/Ljubljana/'
-  NEPREMICNINE_URL = 'http://www.nepremicnine.net/nepremicnine.html?n=1&d=197&p=3&r=14&c1=400&c2=700&s=16'
-  SALOMON_URL ='http://www.salomon.si/oglasi/nepremicnine/stanovanje?q=&mmType=1&filters=1471s-83851x1472s-90256x447m-27555&onPage=100&priceFrom=400&priceTo=700'
+  BOLHA_URL = 'http://www.bolha.com/nepremicnine/stanovanja?adTypeH=02_Oddam/&location=Osrednjeslovenska/Ljubljana/&viewType=30&priceSortField=399%7C700&hasImages=Oglasi%20s%20fotografijami'
+  NEPREMICNINE_URL = 'http://www.nepremicnine.net/nepremicnine.html?n=1&d=197&p=3&r=14&c1=399&c2=700&s=16'
+  SALOMON_URL ='http://www.salomon.si/oglasi/nepremicnine/stanovanje?q=&mmType=1&filters=1471s-83851x1472s-90256x447m-27555&onPage=100&priceFrom=399&priceTo=700'
 
   def index
     @bolha = bolha
